464e4f3858f1b536fd5cd825b4e40306079672b5
joeylam <joey.cm.lam@gmail.com>
2022-03-27 02:12:35 +0800

    clean up the code

--- src/holdings.py
+++ src/holdings.py
@@ -22,9 +22,3 @@ class holdings:
         finally:
             db.close()
 
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s')
-    logging.info('start')
-    main()
-    logging.info('done')
